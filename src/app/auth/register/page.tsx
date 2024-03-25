7dc0bf6efdcd3c08d531fa7dc477c697447381e4
Camilo López <camiloj.lopez79@gmail.com>
2024-03-25 12:50:49 -0300

    Estructura de directorios y primeras páginas

---
+++ src/app/auth/register/page.tsx
@@ -0,0 +1,10 @@
+import { titleFont } from '@/config';
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="">
+      <h1>Register Page</h1>
+    </main>
+  );
+}
