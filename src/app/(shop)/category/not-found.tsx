9d1ad2201fffa57a2bcd9e099477184ea8746c76
Camilo López <camiloj.lopez79@gmail.com>
2024-03-26 11:42:28 -0300

    Página 404 - Categorías

---
+++ src/app/(shop)/category/not-found.tsx
@@ -0,0 +1,11 @@
+// Next.js
+import NextLink from 'next/link';
+
+export default function() {
+  return (
+    <div>
+      <h1>404 Not Found</h1>
+      <NextLink href='/'>Regresar</NextLink>
+    </div>
+  );
+}
