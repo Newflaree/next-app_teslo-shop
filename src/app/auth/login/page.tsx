dd236f938cc955242976a80eb29eb587bb99345f
Camilo López <camiloj.lopez79@gmail.com>
2024-04-03 16:24:31 -0300

    Login - Registro y Auth Layout

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,10 +1,48 @@
+// Next
+import NextLink from 'next/link';
+// Config
 import { titleFont } from '@/config';
-import Image from "next/image";
 
-export default function Home() {
+export default function () {
   return (
-    <div className="">
-      <h1>Login Page</h1>
+    <div className="flex flex-col min-h-screen pt-32 sm:pt-52">
+
+      <h1 className={ `${ titleFont.className } text-4xl mb-10 text-center` }>Ingresar</h1>
+
+      <div className="flex flex-col">
+
+        <label htmlFor="email">Correo electrónico</label>
+        <input
+          className="px-5 py-2 border bg-gray-200 rounded mb-5"
+          type="email" />
+
+
+        <label htmlFor="email">Contraseña</label>
+        <input
+          className="px-5 py-2 border bg-gray-200 rounded mb-5"
+          type="email" />
+
+        <button
+          
+          className="btn-primary">
+          Ingresar
+        </button>
+
+
+        {/* divisor l ine */ }
+        <div className="flex items-center my-5">
+          <div className="flex-1 border-t border-gray-500"></div>
+          <div className="px-2 text-gray-800">O</div>
+          <div className="flex-1 border-t border-gray-500"></div>
+        </div>
+
+        <NextLink
+          href="/auth/register" 
+          className="btn-secondary text-center">
+          Crear una nueva cuenta
+        </NextLink>
+
+      </div>
     </div>
   );
 }
--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,10 +1,56 @@
+// Next
+import NextLink from 'next/link';
+// Config
 import { titleFont } from '@/config';
-import Image from "next/image";
 
-export default function Home() {
+export default function () {
   return (
-    <div className="">
-      <h1>Register Page</h1>
+    <div className="flex flex-col min-h-screen pt-32 sm:pt-52">
+
+      <h1 className={ `${ titleFont.className } text-4xl mb-10 text-center` }>Nueva Cuenta</h1>
+
+      <div className="flex flex-col">
+
+        <label htmlFor="email">Nombre de usuario</label>
+        <input
+          className="px-5 py-2 border bg-gray-200 rounded mb-5"
+          type="text" />
+
+        <label htmlFor="email">Correo electrónico</label>
+        <input
+          className="px-5 py-2 border bg-gray-200 rounded mb-5"
+          type="email" />
+
+        <label htmlFor="email">Contraseña</label>
+        <input
+          className="px-5 py-2 border bg-gray-200 rounded mb-5"
+          type="password" />
+
+        <label htmlFor="email">Repetir contraseña</label>
+        <input
+          className="px-5 py-2 border bg-gray-200 rounded mb-5"
+          type="password" />
+
+        <button
+          className="btn-primary">
+          Crear cuenta
+        </button>
+
+
+        {/* divisor l ine */ }
+        <div className="flex items-center my-5">
+          <div className="flex-1 border-t border-gray-500"></div>
+          <div className="px-2 text-gray-800">O</div>
+          <div className="flex-1 border-t border-gray-500"></div>
+        </div>
+
+        <NextLink
+          href="/auth/login" 
+          className="btn-secondary text-center">
+          Ya tengo una cuenta
+        </NextLink>
+
+      </div>
     </div>
   );
 }
